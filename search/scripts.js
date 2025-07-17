926071d178c3202c359a701f862df70be88ee238
kuzymuzy <nda148822@gmail.com>
2025-07-18 02:04:44 +0300

    да лень мне комит писать иди на хуй

--- search/scripts.js
+++ search/scripts.js
@@ -3,26 +3,34 @@ const result = document.getElementById("result");
 const searchquery = document.getElementById("search-query");
 const googlelink = document.getElementById("googlelink");
 const google = document.getElementById("google");
+const container = document.getElementById("amity-container");
 const urlParams = new URLSearchParams(window.location.search);
 const q = urlParams.get('q');
 if (q === null || q ===""){
     window.location.href = "/"
-}
-else{
+} else{
     searchquery.value = q
-    if (q === "The cutest witch ever"){
+    if (q.toLowerCase() === "the cutest witch ever" || q.toLowerCase() === "самая милая ведьма на свете"){
         document.title = `${q} - Юндекс: нашлось ${count}`;
         result.textContent = "Конечно же Эмити Блайт!"
-        const img = document.createElement("img");
-        img.src = "/assets/img/AmityBlight.png";
-        img.alt = "Эмити Блайт";
-        img.style.position = "fixed";
-        img.style.top = "200px";
-        img.style.left = "60px";
-        img.style.width = "180px";
-        img.style.borderRadius = "10px";
-        document.body.appendChild(img);
-    } else{
+        const img1 = document.createElement("img");
+        img1.src = "/assets/img/AmityBlight.png";
+        img1.alt = "Эмити Блайт";
+        container.appendChild(img1);
+    }
+    else if (q.toLowerCase() === "amity blight" || q.toLowerCase() === "эмити блайт"){
+        document.title = `${q} - Юндекс: нашлось ${count}`;
+        result.textContent = "Самая очаровательная, милая, красивая, добрая ведьма во всем мире."
+        const img1 = document.createElement("img");
+        img1.src = "/assets/img/AmityBlight.png";
+        img1.alt = "Эмити Блайт";
+        const img2 = document.createElement("img");
+        img2.src = "/assets/img/AmityBlight2.png";
+        img2.alt = "Эмити Блайт";
+        container.appendChild(img1);
+        container.appendChild(img2);
+    }
+    else{
         result.textContent = "По вашему запросу ничего не найдено попробуйте поиск наших конкурентов: "
         google.style.display = "block";
         googlelink.href = `https://www.google.com/search?q=${encodeURIComponent(q)}`;
